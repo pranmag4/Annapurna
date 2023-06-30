6dcf091ea0db773d35c58111b7da30a73400a040
pranmag4 <138257712+pranmag4@users.noreply.github.com>
2023-07-01 03:08:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,119 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Shengdana Chutney",
+    price: 40,
+    colors: [
+      {
+        code: "black",
+        img: "./img/shengdana.jpg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Lasun Chutney",
+    price: 40,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/lasun.jpg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Karla Chutney",
+    price: 40,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/karla.jpg",
+      },
+    ],
+  },
+  {
+    id: 4,
+   title: "Javas Chutney",
+    price: 40,
+    colors: [
+      {
+        code: "black",
+        img: "./img/javas.jpg",
+      },
+      
+    ],
+  },
+  {
+    id: 5,
+    title: "Khobara Chutney",
+    price: 40,
+    colors: [
+      {
+        code: "gray",
+        img: "./img/khobara.jpg",
+      },
+      
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent =  choosenProduct.price + "RS";
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
